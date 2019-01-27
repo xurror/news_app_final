f885096c36d4e901dc6ca4fe6f528da45ced117e
xurror <kaze.nasser@gmail.com>
2019-01-27 21:43:38 +0000

    settings.databases

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -84,13 +84,11 @@ WSGI_APPLICATION = 'newsapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
